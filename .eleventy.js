db94d0dde9c9460de393753662087b707540e9c2
takim-13 <takim.doruk@gmail.com>
2026-01-22 21:14:25 +0000

    feat: add social share image and meta tags

--- .eleventy.js
+++ .eleventy.js
@@ -21,6 +21,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/favicon-32x32.png": "favicon-32x32.png" });
   eleventyConfig.addPassthroughCopy({ "src/favicon-16x16.png": "favicon-16x16.png" });
   eleventyConfig.addPassthroughCopy({ "src/apple-touch-icon.png": "apple-touch-icon.png" });
+  eleventyConfig.addPassthroughCopy({ "src/og-image.png": "og-image.png" });
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   eleventyConfig.addFilter("date", formatDate);
 
